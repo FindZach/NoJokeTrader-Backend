7c913288a3ab47ea61de83c197ffa00a617b478b
Zach <zach@findzach.com>
2025-10-22 02:41:54 -0400

    added repo tag

--- src/main/java/org/findzach/trader/repository/DisclosureRepository.java
+++ src/main/java/org/findzach/trader/repository/DisclosureRepository.java
@@ -2,6 +2,7 @@ package org.findzach.trader.repository;
 
 import org.findzach.trader.model.Disclosure;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
@@ -9,6 +10,7 @@ import java.util.Optional;
  * @author Zach Smith
  * @since 10/21/2025
  */
+@Repository
 public interface DisclosureRepository extends JpaRepository<Disclosure, Long> {
     /**
      * Queries for Disclosure based on DocID
